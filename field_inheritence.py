5064b424d1cb2e18c28bb2d088d5c01759aaa1fb
Sascha Strand <sascha.strand@me.com>
2018-03-15 11:27:10 -0700

    Create field_inheritence.py

---
+++ field_inheritence.py
@@ -0,0 +1,18 @@
+class Person():
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    def print_contact(self):
+        print("Name: {}\nEmail: {}".format(self.name, self.email))
+
+class Student(Person):
+    def __init__(self, name, email, stu_id):
+        super().__init__(name, email)
+        self.stu_id = stu_id
+    def print_id(self):
+        print("ID: {}".format(self.stu_id))
+
+max = Student("Max", "max@pdx.edu", "11235")
+
+max.print_contact()
+max.print_id()
